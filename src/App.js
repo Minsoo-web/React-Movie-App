393472ac22027fcd19c12ea23f5f96f9327225ab
minsoo kim <zlemzlem5656@naver.com>
2021-05-21 23:45:05 +0900

    [추가] proptypes 추가 및 class 형 컴포넌트 공부 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div className="App"></div>;
+import React from "react";
+
+class App extends React.Component {
+  state = { count: 0 };
+
+  add = number => {
+    console.log(number);
+  };
+
+  minus = () => {
+    console.log("object");
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => this.add(count)}>+</button>
+        <button onClick={this.minus}>+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
